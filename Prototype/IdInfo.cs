01e3c9d76e65db882d8f930469cb222242dd1607
Henry Paez <hpaezgutierrez@gmail.com>
2022-05-31 16:52:23 -0500

    Prototype and Singleton

---
+++ Prototype/IdInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace Prototype
+{
+    class IdInfo
+    {
+        public int IdNumber;
+
+        public IdInfo(int idNumber)
+        {
+            this.IdNumber = idNumber;
+        }
+    }
+}
---
+++ Prototype/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Prototype
+{
+    class Person
+    {
+        private string _nickname;
+        public int Age;
+        public DateTime BirthDate;
+        public string Name;
+        public IdInfo IdInfo;
+
+        public Person(string name)
+        {
+            this.Name = name;
+            _nickname = $"guest_{name}";
+        }
+
+        public Person ShallowCopy()
+        {
+            return (Person)this.MemberwiseClone();
+        }
+
+        public Person DeepCopy()
+        {
+            Person clone = (Person)this.MemberwiseClone();
+            clone.IdInfo = new IdInfo(IdInfo.IdNumber);
+            clone.Name = String.Copy(Name);
+            return clone;
+        }
+    }
+}
