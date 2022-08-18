436f8180c4028f2bb86e27dbf3c44560194d389a
Henry Paez <hpaezgutierrez@gmail.com>
2022-08-18 17:01:39 -0500

    Decorator, facade and Strategy

---
+++ Strategy/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ctx = new CompressionContext();
+            Console.WriteLine("Please enter Fortmat Type : Rar or Zip");
+            string fortmatType = Console.ReadLine();
+            Console.WriteLine("Fortmat type is : " + fortmatType);
+            
+            if ("Zip".Equals(fortmatType, StringComparison.InvariantCultureIgnoreCase))
+            {
+                ctx.SetStrategy(new ZipCompression());
+            }
+            else if ("Rar".Equals(fortmatType, StringComparison.InvariantCultureIgnoreCase))
+            {
+                ctx.SetStrategy(new RarCompression());
+            }
+
+            ctx.CreateArchive("DotNetDesignPattern");
+            Console.Read();
+        }
+    }
+}
