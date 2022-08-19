69122d23f9c46b3028a0f86f3f775534a23a8a90
Henry Paez <hpaezgutierrez@gmail.com>
2022-08-19 16:09:11 -0500

    Flyweight (Cache-reducing memory)

---
+++ Flyweight/ShapeFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Flyweight
+{
+    public class ShapeFactory
+    {
+        private static Dictionary<string, IShape> shapeMap = new Dictionary<string, IShape>();
+        public static IShape GetShape(string shapeType)
+        {
+            IShape shape = null;
+            if (shapeType.Equals("circle", StringComparison.InvariantCultureIgnoreCase) && !shapeMap.TryGetValue("circle", out shape))
+            {
+                shape = new Circle();
+                shapeMap.Add("circle", shape);
+                Console.WriteLine(" Creating circle object with out any color in shapefactory \n");
+            }
+
+            return shape;
+        }
+    }
+}
